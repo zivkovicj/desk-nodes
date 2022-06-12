614a9e9771273c20ac33ab14ac3ae9342e050ba5
DomenickD <deothe1@gmail.com>
2022-06-12 10:57:26 -0400

    Added a data file

---
+++ data.js
@@ -0,0 +1,14 @@
+const students = [
+    {
+        first_name: "Domenick",
+        last_name: "Dobbs"
+    },
+    {
+        first_name: "Zee",
+        last_name: "Zivkovic"
+    },
+    {
+        first_name: "John",
+        last_name: "Doe"
+    }
+]
